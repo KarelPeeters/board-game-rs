4f6b263d25a4b1f51aad50b6aca08a78240dbbf1
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-01-16 00:08:37 +0100

    Increase stack size to 1024GB and hope the OS lazily allocates.

--- src/bin/solve_ataxx.rs
+++ src/bin/solve_ataxx.rs
@@ -2,7 +2,7 @@ use board_game::solve_ataxx;
 
 fn main() {
     std::thread::Builder::new()
-        .stack_size(1024 * 1024 * 1024)
+        .stack_size(1024 * 1024 * 1024 * 1024)
         .spawn(solve_ataxx::main)
         .unwrap()
         .join()
