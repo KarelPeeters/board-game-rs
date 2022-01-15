b3ec9bb7681e7709b176abfa96b892d661d130f1
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-01-16 00:14:54 +0100

    Attempt to increase stack size on linux.

--- src/bin/solve_ataxx.rs
+++ src/bin/solve_ataxx.rs
@@ -1,10 +1,19 @@
 use board_game::solve_ataxx;
 
+const STACK_SIZE: usize = 1024 * 1024 * 1024 * 1024;
+
+#[cfg(windows)]
 fn main() {
     std::thread::Builder::new()
-        .stack_size(1024 * 1024 * 1024 * 1024)
+        .stack_size(STACK_SIZE)
         .spawn(solve_ataxx::main)
         .unwrap()
         .join()
         .unwrap()
+}
+
+#[cfg(unix)]
+fn main() {
+    rlimit::setrlimit(rlimit::Resource::STACK, rlimit::RLIM_INFINITY, rlimit::RLIM_INFINITY);
+    solve_ataxx::main();
 }
