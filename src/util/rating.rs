d7a02659e66954926347ef1beff2400715688a6e
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-12-07 15:31:57 +0100

    Fix "negative zero" case in elo_from_wdl.

--- src/util/rating.rs
+++ src/util/rating.rs
@@ -3,5 +3,8 @@ use crate::wdl::WDL;
 /// WDL doesn't have to be normalized yet.
 pub fn elo_from_wdl(wdl: WDL<f32>) -> f32 {
     let score = (wdl.value() / wdl.sum() + 1.0) / 2.0;
-    -400.0 * (1.0 / score - 1.0).log10()
+    let elo = -400.0 * (1.0 / score - 1.0).log10();
+
+    // fix annoying negative zero case
+    elo + 0.0
 }
