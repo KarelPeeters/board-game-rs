334fb841fe1497b9064c6031271950e93b0e3228
Karel Peeters <karel.peeters.leuven@gmail.com>
2023-04-11 00:36:52 +0200

    Initial chains implementation.

---
+++ src/games/go/mod.rs
@@ -0,0 +1,10 @@
+pub use board::*;
+pub use io::*;
+pub use rules::*;
+pub use tile::*;
+
+mod board;
+mod chains;
+mod io;
+mod rules;
+mod tile;
---
+++ src/games/go/tile.rs
@@ -0,0 +1,55 @@
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+pub struct Tile {
+    pub x: u8,
+    pub y: u8,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub const ALL: [Direction; 4] = [Direction::Up, Direction::Down, Direction::Left, Direction::Right];
+}
+
+impl Tile {
+    pub fn new(x: u8, y: u8) -> Self {
+        Self { x, y }
+    }
+
+    pub fn all(size: u8) -> impl Iterator<Item = Tile> {
+        (0..size).flat_map(move |y| (0..size).map(move |x| Tile::new(x, y)))
+    }
+
+    pub fn index(&self, size: u8) -> usize {
+        self.y as usize * size as usize + self.x as usize
+    }
+
+    pub fn all_adjacent(self, size: u8) -> impl Iterator<Item = Tile> + Clone {
+        Direction::ALL
+            .iter()
+            .filter_map(move |&dir| self.adjacent_in(dir, size))
+    }
+
+    pub fn adjacent_in(&self, dir: Direction, size: u8) -> Option<Tile> {
+        let (x, y) = match dir {
+            Direction::Up => (self.x, self.y.checked_add(1)?),
+            Direction::Down => (self.x, self.y.checked_sub(1)?),
+            Direction::Left => (self.x.checked_sub(1)?, self.y),
+            Direction::Right => (self.x.checked_add(1)?, self.y),
+        };
+        if x < size && y < size {
+            Some(Tile::new(x, y))
+        } else {
+            None
+        }
+    }
+
+    pub fn exists(&self, size: u8) -> bool {
+        self.x < size && self.y < size
+    }
+}
