5b3298ecf0d2fc47a81f82cd71871a441df18091
Karel Peeters <karel.peeters.leuven@gmail.com>
2023-07-25 23:54:08 +0200

    WIP chess move parsing.

--- src/games/chess.rs
+++ src/games/chess.rs
@@ -4,8 +4,10 @@ use std::hash::Hash;
 use std::ops::ControlFlow;
 use std::str::FromStr;
 
-use cozy_chess::{BitBoard, Color, FenParseError, File, GameStatus, Move, Piece};
+use cozy_chess::{BitBoard, Color, FenParseError, File, GameStatus, Move, Piece, PieceMoves, Rank, Square};
 use internal_iterator::InternalIterator;
+use lazy_static::lazy_static;
+use regex_lite::Regex;
 
 use crate::board::{
     AllMovesIterator, Alternating, AvailableMovesIterator, Board, BoardDone, BoardMoves, Outcome, PlayError, Player,
@@ -36,32 +38,18 @@ pub struct ChessBoard {
     outcome: Option<Outcome>,
 }
 
-#[derive(Debug)]
-pub struct ParseMoveError {
-    pub board: ChessBoard,
-    pub mv: String,
-    pub kind: ParseMoveErrorKind,
-}
-
-#[derive(Debug)]
-pub enum ParseMoveErrorKind {
-    BoardDone,
-    Unavailable(Move),
-    ParseError,
-}
-
 impl ChessBoard {
     pub fn default_with_rules(rules: Rules) -> Self {
-        Self::new_without_history(InnerBoard::default(), rules)
+        Self::from_inner(InnerBoard::default(), rules)
     }
 
-    pub fn new_without_history_fen(fen: &str, rules: Rules) -> Result<Self, FenParseError> {
-        Ok(Self::new_without_history(InnerBoard::from_fen(fen, false)?, rules))
+    pub fn from_fen(fen: &str, rules: Rules) -> Result<Self, FenParseError> {
+        Ok(Self::from_inner(InnerBoard::from_fen(fen, false)?, rules))
     }
 
     /// Construct a new board from an inner chess board, without any history (including repetition and reset counters).
     /// To get a board _with_ history start from the initial board and play the moves on it instead.
-    pub fn new_without_history(inner: InnerBoard, rules: Rules) -> Self {
+    pub fn from_inner(inner: InnerBoard, rules: Rules) -> Self {
         // TODO get this working correctly
         assert_eq!(
             rules.max_moves_without_pawn_or_capture,
@@ -78,27 +66,14 @@ impl ChessBoard {
         }
     }
 
-    pub fn parse_move(&self, mv_str: &str) -> Result<Move, Box<ParseMoveError>> {
-        let err = |kind| ParseMoveError {
-            board: self.clone(),
-            mv: mv_str.to_owned(),
-            kind,
-        };
-        self.check_done().map_err(|_| err(ParseMoveErrorKind::BoardDone))?;
-
-        // this already ignored any extra characters for us (which is a bit sketchy)
-        let mv = Move::from_str(mv_str).map_err(|_| err(ParseMoveErrorKind::ParseError))?;
-
-        // ensure the move is actually available
-        //   we can unwrap here since we already checked that the board is not done
-        if self.is_available_move(mv).unwrap() {
-            Ok(mv)
-        } else {
-            Err(Box::new(err(ParseMoveErrorKind::Unavailable(mv))))
-        }
+    pub fn parse_move(&self, _: &str) -> Result<Move, Box<ParseSanMoveError>> {
+        // // TODO convert case, try UCI and SAN
+        todo!()
     }
 
     pub fn to_san(&self, mv: Move) -> Result<String, PlayError> {
+        todo!();
+
         self.check_can_play(mv)?;
 
         let piece = match self.inner.piece_on(mv.from).unwrap() {
@@ -150,6 +125,255 @@ impl ChessBoard {
 
     pub fn repetitions(&self) -> u16 {
         self.repetitions
+    }
+}
+
+pub fn move_to_uci_str(mv: Move) -> String {
+    let Move { from, to, promotion } = mv;
+
+    match promotion {
+        None => format!("{}{}", from, to),
+        Some(promotion) => format!("{}{}{}", from, to, promotion),
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct ParseUciMoveError {
+    pub mv: String,
+}
+
+pub fn move_from_uci_str(s: &str) -> Result<Move, ParseUciMoveError> {
+    // TODO castling is broken :(
+    if s.len() == 4 || s.len() == 5 {
+        if let Ok(mv) = Move::from_str(s) {
+            if s.len() == 4 || mv.promotion.is_some() {
+                return Ok(mv);
+            }
+        }
+    }
+
+    Err(ParseUciMoveError { mv: s.to_owned() })
+}
+
+// TODO check, checkmate?
+pub fn move_to_san_str(board: &ChessBoard, mv: Move) -> Result<String, PlayError> {
+    board.check_can_play(mv)?;
+
+    let Move { from, to, promotion } = mv;
+    let inner = board.inner();
+
+    let piece = inner.piece_on(from).unwrap();
+    let captured = inner.piece_on(to);
+
+    // castling, encoded as king capturing own rook
+    if piece == Piece::King && captured == Some(Piece::Rook) && inner.color_on(to) == Some(inner.side_to_move()) {
+        return match to.file() {
+            File::G => Ok("O-O-O".to_owned()),
+            File::C => Ok("O-O".to_owned()),
+            _ => unreachable!(),
+        };
+    }
+
+    let result = if piece != Piece::Pawn {
+        // move piece
+
+        let mut matching_file = 0;
+        let mut matching_rank = 0;
+
+        let mask = inner.colored_pieces(inner.side_to_move(), piece);
+
+        inner.generate_moves_for(mask, |list| {
+            matching_file += (list.to & to.file().bitboard()).len();
+            matching_rank += (list.to & to.rank().bitboard()).len();
+
+            // stop if we already need both
+            matching_file > 1 && matching_rank > 1
+        });
+
+        debug_assert!(matching_file > 0 && matching_rank > 0);
+        let piece_str = piece.to_string().to_ascii_uppercase();
+        let capture_str = if captured.is_some() { "x" } else { "" };
+
+        if matching_file == 1 && matching_rank == 1 {
+            format!("{}{}{}", piece_str, capture_str, to)
+        } else if matching_file == 1 {
+            format!("{}{}{}{}", piece_str, from.file(), capture_str, to)
+        } else if matching_rank == 1 {
+            format!("{}{}{}{}", piece_str, from.rank(), capture_str, to)
+        } else {
+            format!("{}{}{}{}", piece_str, from, capture_str, to)
+        }
+    } else {
+        let promotion_str = promotion.map_or("".to_string(), |p| p.to_string().to_ascii_uppercase());
+
+        if captured.is_some() || from.file() != to.file() {
+            // pawn capture (including en passant)
+            let mut matching_rank = 0;
+            let mask = from.file().bitboard() & inner.colored_pieces(inner.side_to_move(), Piece::Pawn);
+            inner.generate_moves_for(mask, |list| {
+                matching_rank += list.len();
+                matching_rank > 1
+            });
+
+            debug_assert!(matching_rank > 0);
+            if matching_rank == 1 {
+                format!("{}x{}{}", from.file(), to, promotion_str)
+            } else {
+                format!("{}x{}{}", from, to, promotion_str)
+            }
+        } else {
+            // pawn push
+            format!("{}{}", to, promotion_str)
+        }
+    };
+    Ok(result)
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum ParseSanMoveError {
+    Parse,
+
+    NoMatchingMove,
+    MultipleMatchingMoves,
+    CaptureMismatch,
+
+    UnavailableMove(Move),
+    BoardDone,
+}
+
+// TODO check, checkmate?
+pub fn move_from_san_str(board: &ChessBoard, s: &str) -> Result<Move, ParseSanMoveError> {
+    if board.is_done() {
+        return Err(ParseSanMoveError::BoardDone);
+    }
+
+    let mv = if s == "O-O" || s == "O-O-O" {
+        let back_rank = Rank::First.relative_to(board.inner().side_to_move());
+        let rook_file = if s == "O-O" { File::G } else { File::C };
+        Move {
+            from: Square::new(File::E, back_rank),
+            to: Square::new(rook_file, back_rank),
+            promotion: None,
+        }
+    } else if let Some(captures) = REGEX_MOVE_PIECE.captures(s) {
+        println!("Move piece");
+
+        let piece = san_piece_from_str(&captures[1]).unwrap();
+        let from_file = captures.get(2).map(|file| File::from_str(file.as_str()).unwrap());
+        let from_rank = captures.get(3).map(|rank| Rank::from_str(rank.as_str()).unwrap());
+        let capture = captures.get(4).is_some();
+        let to = Square::from_str(&captures[5]).unwrap();
+
+        let from = find_san_square_from(&board, piece, from_file, from_rank, to)?;
+
+        let actual_capture = board.inner().piece_on(to).is_some();
+        if capture != actual_capture {
+            return Err(ParseSanMoveError::CaptureMismatch);
+        }
+
+        Move {
+            from,
+            to,
+            promotion: None,
+        }
+    } else if let Some(captures) = REGEX_MOVE_PAWN_CAPTURE.captures(s) {
+        println!("Pawn capture");
+
+        let from_file = File::from_str(&captures[1]).unwrap();
+        let from_rank = captures.get(2).map(|rank| Rank::from_str(rank.as_str()).unwrap());
+        let to = Square::from_str(&captures[3]).unwrap();
+        let promotion = captures.get(4).map(|piece| san_piece_from_str(piece.as_str()).unwrap());
+
+        let from = find_san_square_from(board, Piece::Pawn, Some(from_file), from_rank, to)?;
+
+        Move { from, to, promotion }
+    } else if let Some(captures) = REGEX_MOVE_PAWN_PUSH.captures(s) {
+        println!("Pawn push");
+
+        let to = Square::from_str(&captures[1]).unwrap();
+        let promotion = captures.get(2).map(|piece| san_piece_from_str(piece.as_str()).unwrap());
+
+        let from = find_san_square_from(&board, Piece::Pawn, None, None, to)?;
+
+        Move { from, to, promotion }
+    } else {
+        return Err(ParseSanMoveError::Parse);
+    };
+
+    match board.is_available_move(mv).unwrap() {
+        true => Ok(mv),
+        false => Err(ParseSanMoveError::UnavailableMove(mv)),
+    }
+}
+
+// Based on <https://www.chessprogramming.org/Algebraic_Chess_Notation#Standard_Algebraic_Notation_.28SAN.29>.
+lazy_static! {
+    static ref REGEX_MOVE_PIECE: Regex = Regex::new("^([KQRBN])([a-h])?([1-8])?(x)?([a-h][1-8])$").unwrap();
+    static ref REGEX_MOVE_PAWN_CAPTURE: Regex = Regex::new("^([a-h])([1-8])?x([a-h][1-8])([QRBN])?$").unwrap();
+    static ref REGEX_MOVE_PAWN_PUSH: Regex = Regex::new("^([a-h][1-8])([QRBN])?$").unwrap();
+}
+
+fn find_san_square_from(
+    board: &ChessBoard,
+    piece: Piece,
+    from_file: Option<File>,
+    from_rank: Option<Rank>,
+    to: Square,
+) -> Result<Square, ParseSanMoveError> {
+    if let (Some(from_file), Some(from_rank)) = (from_file, from_rank) {
+        return Ok(Square::new(from_file, from_rank));
+    }
+
+    let inner = board.inner();
+
+    // build a mask of potential source squares, and only consider those moves
+    let mut mask = inner.colored_pieces(inner.side_to_move(), piece);
+    if let Some(from_file) = from_file {
+        mask &= from_file.bitboard();
+    }
+    if let Some(from_rank) = from_rank {
+        mask &= from_rank.bitboard();
+    }
+
+    let mut result = None;
+    let multiple = inner.generate_moves_for(mask, |list| {
+        let PieceMoves {
+            piece: list_piece,
+            from: list_from,
+            to: list_to,
+        } = list;
+
+        debug_assert_eq!(piece, list_piece);
+        debug_assert!(from_file.map_or(true, |from_file| from_file == list_from.file()));
+        debug_assert!(from_rank.map_or(true, |from_rank| from_rank == list_from.rank()));
+
+        if list_to.has(to) {
+            if result.is_some() {
+                true
+            } else {
+                result = Some(list_from);
+                false
+            }
+        } else {
+            false
+        }
+    });
+
+    if multiple {
+        Err(ParseSanMoveError::MultipleMatchingMoves)
+    } else {
+        result.ok_or(ParseSanMoveError::NoMatchingMove)
+    }
+}
+
+fn san_piece_from_str(s: &str) -> Option<Piece> {
+    match s {
+        "K" => Some(Piece::King),
+        "Q" => Some(Piece::Queen),
+        "R" => Some(Piece::Rook),
+        "B" => Some(Piece::Bishop),
+        "N" => Some(Piece::Knight),
+        _ => None,
     }
 }
 
--- tests/board/chess.rs
+++ tests/board/chess.rs
@@ -1,13 +1,24 @@
-use board_game::board::Board;
-use board_game::games::chess::{ChessBoard, Rules};
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 
-use crate::board::{board_perft_main, board_test_main};
+use cozy_chess::{Move, Piece, Square};
+use internal_iterator::InternalIterator;
+use rand::seq::SliceRandom;
+use rand::Rng;
+
+use board_game::board::{Board, BoardMoves};
+use board_game::games::chess::{
+    move_from_san_str, move_from_uci_str, move_to_san_str, move_to_uci_str, ChessBoard, ParseSanMoveError, Rules,
+};
+use board_game::util::tiny::consistent_rng;
+
+use crate::board::board_perft_main;
 
 //TODO add tests for 50 move and 3-move rule
 
 #[test]
 fn chess_start() {
-    board_test_main(&ChessBoard::default());
+    chess_board_test_main(&ChessBoard::default());
 }
 
 #[test]
@@ -23,43 +34,314 @@ fn chess_en_passant() {
     let capture = board.parse_move("ed6").unwrap();
     assert!(board.is_available_move(capture).unwrap());
 
-    board_test_main(&board);
+    chess_board_test_main(&board);
 }
 
 #[test]
 fn test_parse_castle_white() {
-    let board =
-        ChessBoard::new_without_history_fen("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1", Rules::default())
-            .unwrap();
+    let board = fen_board("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1");
 
-    let short = board.parse_move("O-O").unwrap();
-    assert_eq!(short, board.parse_move("e1h1").unwrap());
-    let long = board.parse_move("O-O-O").unwrap();
-    assert_eq!(long, board.parse_move("e1a1").unwrap());
+    // TODO fix castle encoding, UCI and chess did E1G1 but cozy does E1H1
+
+    // main castle moves
+    let short = mv(Square::E1, Square::G1, None);
+    let long = mv(Square::E1, Square::C1, None);
+
+    // TODO how is castling encoded?
+    assert_eq!(Ok(short), move_from_san_str(&board, "O-O"));
+    assert_eq!(Ok(long), move_from_san_str(&board, "O-O-O"));
+    assert_eq!(Ok("O-O".to_string()), move_to_san_str(&board, short));
+    assert_eq!(Ok("O-O-O".to_string()), move_to_san_str(&board, long));
+
+    // random other checks
+    let rook_c1 = mv(Square::A1, Square::C1, None);
+    assert_eq!(Ok(rook_c1), move_from_san_str(&board, "Rc1"));
+    assert_eq!(Ok("Rc1".to_string()), move_to_san_str(&board, rook_c1));
+
+    // TODO check this again?
+    // let short = board.parse_move("O-O").unwrap();
+    // assert_eq!(short, board.parse_move("e1h1").unwrap());
+    // let long = board.parse_move("O-O-O").unwrap();
+    // assert_eq!(long, board.parse_move("e1a1").unwrap());
+
+    chess_board_test_main(&board);
 }
 
 #[test]
 fn test_parse_castle_black() {
-    let board =
-        ChessBoard::new_without_history_fen("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 0 1", Rules::default())
-            .unwrap();
+    let board = fen_board("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 0 1");
 
-    let short = board.parse_move("O-O").unwrap();
-    assert_eq!(short, board.parse_move("e8h8").unwrap());
-    let long = board.parse_move("O-O-O").unwrap();
-    assert_eq!(long, board.parse_move("e8a8").unwrap());
+    // let short = board.parse_move("O-O").unwrap();
+    // assert_eq!(short, board.parse_move("e8h8").unwrap());
+    // let long = board.parse_move("O-O-O").unwrap();
+    // assert_eq!(long, board.parse_move("e8a8").unwrap());
+
+    chess_board_test_main(&board);
 }
 
 /// Test cases from <https://www.chessprogramming.org/Perft_Results>.
 #[test]
+#[ignore] // TODO un-ignore
 fn chess_perft() {
-    #[rustfmt::skip]
-        board_perft_main(
-        |s| ChessBoard::new_without_history_fen(s, Rules::default()).unwrap(),
+    board_perft_main(
+        fen_board,
         Some(|b: &ChessBoard| b.inner().to_string()),
         vec![
-            ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", vec![1, 20, 400, 8902, 197281, 4865609]),
-            ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", vec![1, 48, 2039, 97862, 4085603]),
+            (
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+                vec![1, 20, 400, 8902, 197281, 4865609],
+            ),
+            (
+                "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+                vec![1, 48, 2039, 97862, 4085603],
+            ),
         ],
     );
+}
+
+#[test]
+fn uci_str_invalid() {
+    assert!(move_from_uci_str("e2e4").is_ok());
+    assert!(move_from_uci_str("e7e8q").is_ok());
+
+    // additional garbage
+    assert_eq!(move_from_uci_str("e2e4_garbage").map_err(|_| ()), Err(()));
+
+    // invalid promotions
+    assert_eq!(move_from_uci_str("e7e8k").map_err(|_| ()), Err(()));
+    assert_eq!(move_from_uci_str("e7e8p").map_err(|_| ()), Err(()));
+}
+
+#[test]
+fn uci_str() {
+    let pairs = vec![
+        // basic
+        ("e2e4", mv(Square::E2, Square::E4, None)),
+        // valid promotions
+        ("e7e8q", mv(Square::E7, Square::E8, Some(Piece::Queen))),
+        ("e7e8r", mv(Square::E7, Square::E8, Some(Piece::Rook))),
+        ("e7e8b", mv(Square::E7, Square::E8, Some(Piece::Bishop))),
+        ("e7e8n", mv(Square::E7, Square::E8, Some(Piece::Knight))),
+        // basic moves that could mean castling
+        ("e1g1", mv(Square::E1, Square::G1, None)),
+        ("e1c1", mv(Square::E1, Square::C1, None)),
+        ("e8g8", mv(Square::E8, Square::G8, None)),
+        ("e8c8", mv(Square::E8, Square::C8, None)),
+    ];
+
+    for (s, m) in pairs {
+        assert_eq!(s, move_to_uci_str(m));
+        assert_eq!(Ok(m), move_from_uci_str(s));
+    }
+}
+
+// TODO steal test cases from https://github.com/jordanbray/chess/blob/0e538fbee63d01b282cd22a40f1b6655cd6922ab/src/chess_move.rs#L61
+
+// TODO make sure castling maps correctly in kZero!
+#[test]
+fn derp() {
+    println!("{:?}", move_from_san_str(&ChessBoard::default(), "K3e2"));
+    println!("{:?}", move_from_san_str(&ChessBoard::default(), "Ke2"));
+}
+
+#[test]
+fn full_san() {
+    let board = fen_board("8/8/8/k6B/6r1/K4B1B/8/8 w - - 0 1");
+
+    let mv = mv(Square::H3, Square::G4, None);
+
+    assert_eq!(Ok(true), board.is_available_move(mv));
+
+    assert_eq!(Ok(mv), move_from_san_str(&board, "Bh3xg4"));
+    assert_eq!(
+        Err(ParseSanMoveError::MultipleMatchingMoves),
+        move_from_san_str(&board, "Bhxg4")
+    );
+    assert_eq!(
+        Err(ParseSanMoveError::MultipleMatchingMoves),
+        move_from_san_str(&board, "B3xg4")
+    );
+    assert_eq!(
+        Err(ParseSanMoveError::MultipleMatchingMoves),
+        move_from_san_str(&board, "Bxg4")
+    );
+
+    assert_eq!(Ok("Bh3xg4"), move_to_san_str(&board, mv).as_ref().map(String::as_str));
+
+    chess_board_test_main(&board);
+}
+
+#[test]
+fn invalid_san() {
+    let board = fen_board("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1");
+    let result = move_from_san_str(&board, "h45bx");
+    assert!(result.is_err(), "Got {:?}, expected err", result);
+}
+
+#[test]
+fn fuzz_boards() {
+    let mut rng = consistent_rng();
+
+    for i in 0..1000 {
+        println!("Start fuzz {}", i);
+
+        let mut board = ChessBoard::default();
+
+        let mut len = 0;
+
+        while board.play_random_available_move(&mut rng).is_ok() {
+            test_move_str_loop(&board);
+            fuzz_parse_random_san_strings(&board);
+
+            len += 1;
+            if len > 1_000 {
+                break;
+            }
+        }
+    }
+}
+
+fn chess_board_test_main(board: &ChessBoard) {
+    // basic tests
+    crate::board::board_test_main(board);
+
+    // move string tests
+    test_move_str_loop(board);
+    fuzz_parse_random_san_strings(board);
+}
+
+const MAX_LEN_UCI: usize = 5;
+const MAX_LEN_SAN: usize = 6;
+const POSSIBLE_CHARS_UCI: &str = "abcdefgh12345678qrbn";
+const POSSIBLE_CHARS_SAN: &str = "abcdefgh12345678KQRBN-Ox";
+
+#[test]
+fn fuzz_parse_random_uci_strings() {
+    let n = 10_000;
+    let mut rng = consistent_rng();
+    let mut uci = String::new();
+
+    for _ in 0..n {
+        uci.clear();
+        for _ in 0..rng.gen_range(0..=MAX_LEN_UCI) {
+            uci.push(*POSSIBLE_CHARS_UCI.as_bytes().choose(&mut rng).unwrap() as char);
+        }
+
+        if let Ok(mv) = move_from_uci_str(&uci) {
+            assert_eq!(uci, move_to_uci_str(mv));
+        }
+    }
+}
+
+fn is_subsequence_of(needle: &str, haystack: &str) -> bool {
+    let mut haystack = haystack.chars();
+
+    'outer: for n in needle.chars() {
+        for h in &mut haystack {
+            if n == h {
+                continue 'outer;
+            }
+        }
+        if haystack.next().is_none() {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn fuzz_parse_random_san_strings(board: &ChessBoard) {
+    let n = 10_000;
+    let mut rng = consistent_rng();
+    let mut san = String::new();
+
+    for _ in 0..n {
+        san.clear();
+        for _ in 0..rng.gen_range(0..=MAX_LEN_SAN) {
+            san.push(*POSSIBLE_CHARS_SAN.as_bytes().choose(&mut rng).unwrap() as char);
+        }
+
+        if let Ok(mv) = move_from_san_str(board, &san) {
+            assert_eq!(Ok(true), board.is_available_move(mv));
+
+            let minimal_san = move_to_san_str(board, mv).unwrap();
+            assert!(
+                is_subsequence_of(&minimal_san, &san),
+                "{:?} -> {:?} -> {:?} is not a subsequence of orig, board: {:?}",
+                san,
+                mv,
+                minimal_san,
+                board
+            );
+        }
+    }
+}
+
+fn test_move_str_loop(board: &ChessBoard) {
+    if let Ok(moves) = board.available_moves() {
+        // move strings
+        let mut uci_moves = HashMap::new();
+        let mut san_moves = HashMap::new();
+
+        let mut any_duplicate = false;
+
+        moves.for_each(|mv| {
+            let uci = move_to_uci_str(mv);
+            let san = move_to_san_str(board, mv)
+                .unwrap_or_else(|e| panic!("Failed to convert {:?} to SAN, error {:?}", mv, e));
+
+            let missing_uci_char = uci.chars().find(|&c| !POSSIBLE_CHARS_UCI.contains(c));
+            assert_eq!(None, missing_uci_char, "{:?}", uci);
+            let missing_san_char = san.chars().find(|&c| !POSSIBLE_CHARS_SAN.contains(c));
+            assert_eq!(None, missing_san_char, "{:?}", san);
+
+            assert!(uci.len() <= MAX_LEN_UCI, "Found longer UCI: {:?}", uci);
+            assert!(san.len() <= MAX_LEN_SAN, "Found longer SAN: {:?}", san);
+
+            let mv_uci = move_from_uci_str(&uci);
+            assert_eq!(Ok(mv), mv_uci, "UCI parse failed: {} -> {:?} -> err", mv, uci);
+            let mv_san = move_from_san_str(board, &san);
+            assert_eq!(Ok(mv), mv_san, "SAN parse failed: {} -> {:?} -> err", mv, san);
+
+            match uci_moves.entry(uci.clone()) {
+                Entry::Occupied(entry) => {
+                    println!(
+                        "Duplicate UCI move string: {} and {} both map to {}",
+                        entry.get(),
+                        mv,
+                        uci
+                    );
+                    any_duplicate = true;
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(mv);
+                }
+            }
+
+            match san_moves.entry(san.clone()) {
+                Entry::Occupied(entry) => {
+                    println!(
+                        "Duplicate SAN move string: {} and {} both map to {}",
+                        entry.get(),
+                        mv,
+                        san
+                    );
+                    any_duplicate = true;
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(mv);
+                }
+            }
+        });
+
+        assert!(!any_duplicate, "Found duplicate move string!");
+    }
+}
+
+fn mv(from: Square, to: Square, promotion: Option<Piece>) -> Move {
+    Move { from, to, promotion }
+}
+
+fn fen_board(s: &str) -> ChessBoard {
+    ChessBoard::from_fen(s, Rules::default()).unwrap()
 }
